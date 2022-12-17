b1e2a4de13e2a8c6c68617025a51de516e2a60bf
Hetansh Madhani <hetanshmadhanidev@gmail.com>
2022-12-17 18:24:48 -0500

    add nav links to route to pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import './App.css';
 
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import PlayerScouter from './pages/PlayerScouter';
 import Player from './pages/Player';
 
 function App() {
   return (
+    <>
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/players">Players</Link></li>
+        <li><Link to="/player/:id">Player Data</Link></li>
+      </ul>
+    </nav>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/players" element={<PlayerScouter />} />
       <Route path="/player/:id" element={<Player />} />
     </Routes>
+    
+    </>
   );
 }
 
